b1e5fa070f067d69a39cf95b9dc841d8542f3ce0
ChallengerCY <790597004@qq.com>
2020-01-08 00:42:40 +0800

    编写AssetBundle简单例子

---
+++ AssetBundleDemo/Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using System.IO;
+
+/// <summary>
+/// 创建AssetBundle文件
+/// </summary>
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        //设置assetBundle的根路径
+        string assetBundleDirectory = "Assets/AssetBundles";
+        //判断文件夹是否存在
+        if(!Directory.Exists(assetBundleDirectory))
+        {
+            //如果不存在则创建文件夹
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        
+        //调用Assetbundle构建API 
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, 
+            BuildAssetBundleOptions.None, 
+            BuildTarget.StandaloneOSX);
+    }
+}
---
+++ AssetBundleDemo/Assets/Script/ResManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ResManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadFromLocalFile();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    /// <summary>
+    /// 从本地加载AssetBundle资源
+    /// </summary>
+    void LoadFromLocalFile() {
+        
+        //这里要传入指定AssetBundle所在的位置。
+        var myLoadedAssetBundle 
+            = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "cube"));
+        if (myLoadedAssetBundle == null) {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+        //通过对象名字来实例化对象。
+        var prefab = myLoadedAssetBundle.LoadAsset<GameObject>("cube");
+        Instantiate(prefab);
+        
+      
+    }
+}
