6fda48b5d0719b04109261b93e34fb0be9ba22bd
ChallengerCY <790597004@qq.com>
2020-01-24 14:25:35 +0800

    编写AssetBundle打包脚本

--- AssetBundleDemo/Assets/Editor/CreateAssetBundles.cs
+++ AssetBundleDemo/Assets/Editor/CreateAssetBundles.cs
@@ -6,11 +6,14 @@ using System.IO;
 /// </summary>
 public class CreateAssetBundles
 {
-    [MenuItem("Assets/Build AssetBundles")]
-    static void BuildAllAssetBundles()
+    /// <summary>
+    /// 创建mac系统用的AssetBundle
+    /// </summary>
+    [MenuItem("Assets/Build AssetBundles/macOS")]
+    static void BuildMacOsAllAssetBundles()
     {
         //设置assetBundle的根路径
-        string assetBundleDirectory = "Assets/AssetBundles";
+        string assetBundleDirectory = "Assets/StreamingAssets/AssetBundles/MacOS";
         //判断文件夹是否存在
         if(!Directory.Exists(assetBundleDirectory))
         {
@@ -22,5 +25,55 @@ public class CreateAssetBundles
         BuildPipeline.BuildAssetBundles(assetBundleDirectory, 
             BuildAssetBundleOptions.None, 
             BuildTarget.StandaloneOSX);
+        
+        AssetDatabase.Refresh();
     }
+    
+    /// <summary>
+    /// 创建android系统用的Assetbundle
+    /// </summary>
+    [MenuItem("Assets/Build AssetBundles/android")]
+    static void BuildAndroidAllAssetBundles()
+    {
+        //设置assetBundle的根路径
+        string assetBundleDirectory = "Assets/StreamingAssets/AssetBundles/Android";
+        //判断文件夹是否存在
+        if(!Directory.Exists(assetBundleDirectory))
+        {
+            //如果不存在则创建文件夹
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        
+        //调用Assetbundle构建API 
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, 
+            BuildAssetBundleOptions.None, 
+            BuildTarget.Android);
+        
+        AssetDatabase.Refresh();
+    }
+    
+    /// <summary>
+    /// 创建ios系统用的Assetbundle
+    /// </summary>
+    [MenuItem("Assets/Build AssetBundles/ios")]
+    static void BuildIosAllAssetBundles()
+    {
+        //设置assetBundle的根路径
+        string assetBundleDirectory = "Assets/StreamingAssets/AssetBundles/Ios";
+        //判断文件夹是否存在
+        if(!Directory.Exists(assetBundleDirectory))
+        {
+            //如果不存在则创建文件夹
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        
+        //调用Assetbundle构建API 
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, 
+            BuildAssetBundleOptions.None, 
+            BuildTarget.iOS);
+        
+        AssetDatabase.Refresh();
+    }
+    
+    
 }
